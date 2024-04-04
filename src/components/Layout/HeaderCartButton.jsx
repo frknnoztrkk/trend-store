41aff61aba6788d7a9bda1f8ea1eb69e8172c7e5
Furkan Öztürk <furkan25ozturk@gmail.com>
2024-04-05 02:08:55 +0300

    Regeneration-ReactPortal

--- src/components/Layout/HeaderCartButton.jsx
+++ src/components/Layout/HeaderCartButton.jsx
@@ -1,16 +1,24 @@
-import React from 'react'
-import CartIcon from '../Cart/CartIcon'
+import { useContext } from "react";
+import CartIcon from "../Cart/CartIcon";
+import { CartContext } from "../../context/CartProvider";
+import "./HeaderCartButton.css";
 
-const HeaderCartButton = ({onShowCart}) => {
+const HeaderCartButton = ({ onShowCart }) => {
+  const cartCtx = useContext(CartContext);
+
+  const totalItemsCart = cartCtx.items.reduce((accumulator, currentItem) => {
+    return accumulator + currentItem.amount;
+  }, 0);
+
   return (
-    <button className='button' onClick={onShowCart}>
-        <span className='icon'>
-            < CartIcon />
-        </span>
-        <span>Sepetim</span>
-        <span className='badge'>0</span>
+    <button className="button" onClick={onShowCart}>
+      <span className="icon">
+        <CartIcon />
+      </span>
+      <span>Sepetim</span>
+      <span className="badge">{totalItemsCart}</span>
     </button>
-  )
-}
+  );
+};
 
-export default HeaderCartButton
+export default HeaderCartButton;
--- src/components/UI/Offcanvas.jsx
+++ src/components/UI/Offcanvas.jsx
@@ -1,21 +1,21 @@
 import { Fragment } from "react";
-import { ReactDOM } from "react-dom";
+import ReactDOM from "react-dom";
 
 import "./Offcanvas.css";
 
+const Backdrop = (props) => {
+  return <div className="backdrop" onClick={props.onClose} />;
+};
+
+const OffcanvasOverlay = (props) => {
+  return (
+    <div className="offcanvas">
+      <div className="content">{props.children}</div>
+    </div>
+  );
+};
+
 const Offcanvas = (props) => {
-  const Backdrop = (props) => {
-    return <div className="backdrop" onClick={props.onClose}></div>;
-  };
-
-  const OffcanvasOverlay = (props) => {
-    return (
-      <div className="offcanvas">
-        <div className="content">{props.children}</div>
-      </div>
-    );
-  };
-
   const portalElement = document.getElementById("overlays");
   return (
     <Fragment>
@@ -23,7 +23,6 @@ const Offcanvas = (props) => {
         <Backdrop onClose={props.onClose} />,
         portalElement
       )}
-
       {ReactDOM.createPortal(
         <OffcanvasOverlay>{props.children}</OffcanvasOverlay>,
         portalElement
