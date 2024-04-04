cb3445dc7677a03f854fd62949928c13933cbe2d
Furkan Öztürk <furkan25ozturk@gmail.com>
2024-04-05 01:18:53 +0300

    ReactPortal

--- src/components/UI/Offcanvas.jsx
+++ src/components/UI/Offcanvas.jsx
@@ -1,12 +1,33 @@
 import { Fragment } from "react";
+import { ReactDOM } from "react-dom";
+
 import "./Offcanvas.css";
+
 const Offcanvas = (props) => {
-  return (
-    <Fragment>
-      <div className="backdrop" onClick={props.onClose}></div>
+  const Backdrop = (props) => {
+    return <div className="backdrop" onClick={props.onClose}></div>;
+  };
+
+  const OffcanvasOverlay = (props) => {
+    return (
       <div className="offcanvas">
         <div className="content">{props.children}</div>
       </div>
+    );
+  };
+
+  const portalElement = document.getElementById("overlays");
+  return (
+    <Fragment>
+      {ReactDOM.createPortal(
+        <Backdrop onClose={props.onClose} />,
+        portalElement
+      )}
+
+      {ReactDOM.createPortal(
+        <OffcanvasOverlay>{props.children}</OffcanvasOverlay>,
+        portalElement
+      )}
     </Fragment>
   );
 };
